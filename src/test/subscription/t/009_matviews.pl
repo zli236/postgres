b193e29c362aaf379012c2fc6c2e4e648d1c0bb0
Zheng (Zane) Li <zhelli@amazon.com>
2022-03-18 17:17:39 +0000

    Logical replication of DDL messages
    Integration with pgoutput:
    Supports sending and receiving the DDL message using the logical
    replication wire protocol. A new LogicalRepMsgType is introduced
    for this purpose: LOGICAL_REP_MSG_DDLMESSAGE = 'L'.
    
    Logical replication worker change:
    Supports execution of the DDL command in the original user role and
    search_path. For any new table created this way, we also set its
    srsubstate in the pg_subscription_rel catalog to SUBREL_STATE_INIT,
    So that DML replication can progress on this new table without
    manually running "ALTER SUBSCRIPTION ... REFRESH PUBLICATION".
    
    TAP test:
    A new TAP test 030_rep_ddl.pl is added. We mainly focused on
    testing the happy path of database level replication so far.
    Corner case DDLs and table level DDL replication are still to be
    carefully tested.

--- src/test/subscription/t/009_matviews.pl
+++ src/test/subscription/t/009_matviews.pl
@@ -19,7 +19,7 @@ $node_subscriber->start;
 my $publisher_connstr = $node_publisher->connstr . ' dbname=postgres';
 
 $node_publisher->safe_psql('postgres',
-	"CREATE PUBLICATION mypub FOR ALL TABLES;");
+	"CREATE PUBLICATION mypub FOR ALL TABLES WITH (ddl = '');");
 $node_subscriber->safe_psql('postgres',
 	"CREATE SUBSCRIPTION mysub CONNECTION '$publisher_connstr' PUBLICATION mypub;"
 );
